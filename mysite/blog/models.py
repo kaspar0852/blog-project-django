7a519e039a1297e468587dc66fae72cc9fd11015
saugat <sanepalaaye@gmail.com>
2023-01-05 09:02:12 +0545

    a

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from djaongo.utils import timezone
+from django.utils import timezone
 from django.core.urlresolvers import reverse
 
 # Create your models here.
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView,ListView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 from blog.models import *
+from blog.forms import *
+
 # Create your views here.
 
 class AboutView(TemplateView):
@@ -15,4 +19,36 @@ class PostListView(ListView):
 
         #from get_queryset we are going to do a query like a sql query-
         #we are basically saying grab the post model and filter out based on the given conditions
-        
+
+class PostDetailedView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+#i dont want anyone to access this view
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    
+    form_class = PostForm
+    
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    
+    form_class = PostForm
+    
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
