fd2c067f48fde8b01da2db073af350ac00fe806c
saugat <sanepalaaye@gmail.com>
2023-01-06 08:56:26 +0545

    views and urls done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=265)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
@@ -26,7 +26,7 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name='comments')
+    post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE)
     author = models.CharField(max_length=265)
     text = models.TextField()
     create_date = models.DateTimeField(default = timezone.now())
